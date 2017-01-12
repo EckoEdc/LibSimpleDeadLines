ffb1d833180f46a79cbc330cb065163cd65a23cc
Edric Milaret <edric.milaret@gmail.com>
2017-01-12 10:49:21 -0500

    init AERecord

--- LibSimpleDeadlines/TasksService.swift
+++ LibSimpleDeadlines/TasksService.swift
@@ -10,9 +10,22 @@ import Foundation
 import AERecord
 
 public class TasksService {
+    
     public static var sharedInstance = TasksService()
     
     init() {
-        
+        guard let modelURL = Bundle.main.url(forResource: "SimpleDeadlinesModel", withExtension:"momd") else {
+            fatalError("Error loading model from bundle")
+        }
+        guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Error initializing mom from: \(modelURL)")
+        }
+        let myStoreURL = AERecord.storeURL(for: "SimpleDeadlinesModel")
+        let myOptions = [NSMigratePersistentStoresAutomaticallyOption : true, NSInferMappingModelAutomaticallyOption: true]
+        do {
+            try AERecord.loadCoreDataStack(managedObjectModel: mom, storeURL: myStoreURL, options: myOptions)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
     }
 }
