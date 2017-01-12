8d8c4932d171d5771f4b71460d701bedf05759cb
Edric MILARET <edric@MBP-de-Edric.local>
2017-01-12 10:27:30 -0500

    first commit

---
+++ LibSimpleDeadlines/LibSimpleDeadlines.h
@@ -0,0 +1,19 @@
+//
+//  LibSimpleDeadlines.h
+//  LibSimpleDeadlines
+//
+//  Created by Edric MILARET on 17-01-12.
+//  Copyright © 2017 Edric MILARET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibSimpleDeadlines.
+FOUNDATION_EXPORT double LibSimpleDeadlinesVersionNumber;
+
+//! Project version string for LibSimpleDeadlines.
+FOUNDATION_EXPORT const unsigned char LibSimpleDeadlinesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibSimpleDeadlines/PublicHeader.h>
+
+
