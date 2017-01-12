8e3eaab854863e300edf72b6fcac7f01e5babbdd
Edric MILARET <edric@mbp-de-edric.local>
2017-01-12 11:34:17 -0500

    fix bundle find

--- LibSimpleDeadlines/TasksService.swift
+++ LibSimpleDeadlines/TasksService.swift
@@ -14,16 +14,11 @@ public class TasksService {
     public static var sharedInstance = TasksService()
     
     init() {
-        guard let modelURL = Bundle.main.url(forResource: "SimpleDeadlinesModel", withExtension:"momd") else {
-            fatalError("Error loading model from bundle")
-        }
-        guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
-            fatalError("Error initializing mom from: \(modelURL)")
-        }
-        let myStoreURL = AERecord.storeURL(for: "SimpleDeadlinesModel")
-        let myOptions = [NSMigratePersistentStoresAutomaticallyOption : true, NSInferMappingModelAutomaticallyOption: true]
+        let model: NSManagedObjectModel = AERecord.modelFromBundle(for: TasksService.self)
+        let storeURL = AERecord.storeURL(for: "SimpleDeadlinesModel")
+        let options = [NSMigratePersistentStoresAutomaticallyOption : true, NSInferMappingModelAutomaticallyOption: true]
         do {
-            try AERecord.loadCoreDataStack(managedObjectModel: mom, storeURL: myStoreURL, options: myOptions)
+            try AERecord.loadCoreDataStack(managedObjectModel: model, storeURL: storeURL, options: options)
         } catch {
             fatalError(error.localizedDescription)
         }
